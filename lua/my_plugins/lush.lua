84b15cf9d12f1be629c29b275b8100b7d71ef567
Longhan9926 <allenryan9926@gmail.com>
2024-04-10 09:45:33 +0800

    init

---
+++ lua/my_plugins/lush.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        'rktjmp/lush.nvim'
+    }
+    -- Your Plugin1
+	--  {
+	--      'hardhacker/plugin1',
+	--      config = function() ... end
+	--  },
+
+	-- Your Plugin2
+	--  {'hardhacker/plugin2'},
+}
